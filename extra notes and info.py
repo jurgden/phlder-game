05be724c81cf016b6a8e9e755c777b5b47f50ba0
jurgden <82841924+jurgden@users.noreply.github.com>
2022-08-15 20:26:08 -0700

    added bonus dev info for future development

---
+++ extra notes and info.py
@@ -0,0 +1,8 @@
+# This file will contain useful development information for future use such as HEX values for color palletes etc.
+
+# Color Pallete!
+# Primary Background color : #050505 "A kind of dark moody black" RGB VAL : 050505
+# Primary Foreground color : #1B9AAA "A sky blue with a retro feel" RGB VAL : 27154170
+# Secondary Background color : #DDDBCB "Bone white" RGB VAL : 221219203
+# Secondary Foreground color : #F5F1E3 "Old lace beige" RGB VAL : 245241227
+# All other accents and details : #FFFFFF "Classic white" RGB VAL : 255255255
